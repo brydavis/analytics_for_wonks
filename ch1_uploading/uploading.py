dbb0aa5c2e850222cb039ba4171e20d59e5ece13
Bryan Davis <bryan@stax.io>
2016-08-03 15:10:22 -0700

    Update uploading.py

--- ch1_uploading/uploading.py
+++ ch1_uploading/uploading.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 data = pd.read_csv("C:/Users/jsmith/my_data.csv")
 
-data.head(20) # prints out first 20 rows of data
+print(data.head(20)) # prints out first 20 rows of data
 
-data.describe() # gives quick descriptive stats on each in columns
+print(data.describe()) # gives quick descriptive stats on each in columns
+
+print(data.dtypes) # lists each column and current data types (e.g. int, float, date)
