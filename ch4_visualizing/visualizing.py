d653bd3acdc1faa3cc575afb8776cb0ea270b1d1
Bryan Davis <bryan@stax.io>
2016-08-03 15:13:01 -0700

    Update visualizing.py

--- ch4_visualizing/visualizing.py
+++ ch4_visualizing/visualizing.py
@@ -1,1 +1,22 @@
-#
+# Ch1 - Uploading Data
+import pandas as pd
+
+data = pd.read_csv("C:/Users/jsmith/my_data.csv")
+
+print(data.head(20)) # prints out first 20 rows of data
+
+print(data.describe()) # gives quick descriptive stats on each in columns
+
+print(data.dtypes) # lists each column and current data types (e.g. int, float, date)
+
+
+# Ch4 - Visualizing Data
+%matplotlib inline # only if you're using Jupyter
+
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+city_groups = data.groupby('city').size()
+city_groups.hist()
